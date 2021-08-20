6af7b3a2f903236780e0ef14a95ecac8c35a9c06
Mauricio Ulate <mauricio.ulate@prodigious.com>
2021-08-20 14:17:50 -0600

    Se agregó ejemplo de input

---
+++ 01-hola-mundo/input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    
+    string input;
+    cout << "Ingrese su nombre: ";
+    cin >> input;
+    cout << "Hola " << input << endl;
+    return 0;
+}
